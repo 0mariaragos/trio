4b2ce96d8eefaa1894a5c7a424375dcf74090d90
mathsluz97 <maths.luzsantana97@gmail.com>
2019-10-21 22:18:59 -0300

    segundo commit subtrair

--- teste/Program.cs
+++ teste/Program.cs
@@ -12,6 +12,10 @@ namespace teste
         {
             return valor1 + valor2;
         }
+        public static int subtrair(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
         static void Main(string[] args)
         {
         }
