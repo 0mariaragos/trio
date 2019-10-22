7069b066bf8b050f3170d4da897e1a5b2282630c
0mariaragos <mariana.aragos@etec.sp.gov.br>
2019-10-21 22:05:02 -0300

    primeiro commit

---
+++ teste/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    class Program
+    {
+        public static int somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
