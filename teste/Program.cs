771694d81df7474a4d00ae4cc90367974f3c6c5f
marceloroella <marceloroella@gmail.com>
2019-10-21 22:28:19 -0300

    commit 3 multiplicar

--- teste/Program.cs
+++ teste/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace teste
 {
     class Program
-    {
+    {C:\Users\aluno3\Desktop\trio\teste\Program.cs
         public static int somar(int valor1, int valor2)
         {
             return valor1 + valor2;
@@ -15,6 +15,10 @@ namespace teste
         public static int subtrair(int valor1, int valor2)
         {
             return valor1 - valor2;
+        }
+        public static int multiplicar(int valor1,int valor 2)
+        {
+            return valor1 * valor2;
         }
         static void Main(string[] args)
         {
