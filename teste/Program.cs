1da456a61b4687930a7dfb5f5d593841cc1f21e9
marceloroella <marceloroella@gmail.com>
2019-11-04 23:41:05 -0200

    multiplicar

--- teste/Program.cs
+++ teste/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 namespace teste
 {
     class Program
-    {C:\Users\aluno3\Desktop\trio\teste\Program.cs
         public static int somar(int valor1, int valor2)
         {
             return valor1 + valor2;
@@ -16,7 +15,7 @@ namespace teste
         {
             return valor1 - valor2;
         }
-        public static int multiplicar(int valor1,int valor 2)
+        public static int multiplicar(int valor1, int valor 2)
         {
             return valor1 * valor2;
         }
@@ -25,6 +24,8 @@ namespace teste
             Console.WriteLine("O resultado da soma: {0} + {1} = {2}", 3, 5, somar(3, 5));
 
             Console.WriteLine("O resultado da subtração: {0} + {1} = {2}", 3, 5, subtrair(3, 5));
+
+            Console.WriteLine("O resultado da multiplicação: {0} + {1} = {2}", 3, 5, multiplicar(3, 5));
         }
     }
-}
+
